2752fc21c4d2f94cd05525bfd36855381b050687
rodrigokishi <rodrigokishi@usp.br>
2016-09-13 18:49:59 -0300

    Testing remote modification via git

--- src/main/java/intermidia/SIFTExtractor.java
+++ src/main/java/intermidia/SIFTExtractor.java
@@ -7,6 +7,7 @@ public class SIFTExtractor
     public static void main( String[] args ) 
     {
     	Keyframe kf;
+    	System.out.println("maquina do lab");
     	System.out.println("test");
     }
 }
